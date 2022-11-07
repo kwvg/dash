09b88ed3762987692e32265032c5a605ed226866
Kittywhiskers Van Gogh <63189531+kittywhiskers@users.noreply.github.com>
2022-11-07 13:08:11 +0530

    ci: import missing 00_setup_env_native_asan.sh from https://github.com/bitcoin/bitcoin/commit/faa8023ce9a47b282e1fac3ca8b3a7bb0042935a

---
+++ ci/test/00_setup_env_native_asan.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+#
+# Copyright (c) 2019 The Bitcoin Core developers
+# Distributed under the MIT software license, see the accompanying
+# file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+export LC_ALL=C.UTF-8
+
+export PACKAGES="clang-8 llvm-8 python3-zmq qtbase5-dev qttools5-dev-tools libevent-dev bsdmainutils libboost-system-dev libboost-filesystem-dev libboost-chrono-dev libboost-test-dev libboost-thread-dev libdb5.3++-dev libminiupnpc-dev libzmq3-dev libqrencode-dev"
+export DEP_OPTS="NO_UPNP=1 DEBUG=1"
+export CPPFLAGS="-DDEBUG_LOCKORDER -DENABLE_DASH_DEBUG -DARENA_DEBUG"
+# Use clang-8 instead of default clang (which is clang-6 on Bionic) to avoid spurious segfaults when running on ppc64le
+export GOAL="install"
+export BITCOIN_CONFIG="--enable-zmq --with-incompatible-bdb --with-gui=qt5 --with-sanitizers=address,integer,undefined CC=clang-8 CXX=clang++-8"
