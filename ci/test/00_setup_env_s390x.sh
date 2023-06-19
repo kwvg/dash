53bae47ff7fcdae3b063ff0944d02e6731f1a961
Kittywhiskers Van Gogh <63189531+kittywhiskers@users.noreply.github.com>
2023-06-19 14:41:12 +0000

    ci: use Debian 10 (buster) instead of Ubuntu 20.04 (focal)

--- ci/test/00_setup_env_s390x.sh
+++ ci/test/00_setup_env_s390x.sh
@@ -18,7 +18,7 @@ if [ -n "$QEMU_USER_CMD" ]; then
 fi
 # Use debian to avoid 404 apt errors
 export CONTAINER_NAME=ci_s390x
-export DOCKER_NAME_TAG="debian:focal"
+export DOCKER_NAME_TAG="debian:buster"
 export RUN_UNIT_TESTS=true
 export RUN_FUNCTIONAL_TESTS=true
 export GOAL="install"
