0979b067be35a204dcd1866859171bd699fd18f8
Kittywhiskers Van Gogh <63189531+kwvg@users.noreply.github.com>
2024-05-14 18:43:13 +0000

    net: make `Sock::Wait()` subject to selection by `-socketevents`

--- src/util/sock.cpp
+++ src/util/sock.cpp
@@ -153,7 +153,41 @@ int Sock::GetSockName(sockaddr* name, socklen_t* name_len) const
 
 bool Sock::Wait(std::chrono::milliseconds timeout, Event requested, Event* occurred) const
 {
+    std::string debug_str;
+
+    switch (m_event_mode)
+    {
 #ifdef USE_POLL
+        case SocketEventsMode::Poll:
+            return WaitPoll(timeout, requested, occurred);
+#endif /* USE_POLL */
+        case SocketEventsMode::Select:
+            return WaitSelect(timeout, requested, occurred);
+        case SocketEventsMode::EPoll:
+            debug_str += "Unimplemented for epoll, falling back on ";
+            break;
+        case SocketEventsMode::KQueue:
+            debug_str += "Unimplemented for kqueue, falling back on ";
+            break;
+        default:
+            assert(false);
+    }
+#ifdef USE_POLL
+    debug_str += "poll";
+#else
+    debug_str += "select";
+#endif /* USE_POLL*/
+    LogPrintf("%s\n", debug_str);
+#ifdef USE_POLL
+    return WaitPoll(timeout, requested, occurred);
+#else
+    return WaitSelect(timeout, requested, occurred);
+#endif /* USE_POLL */
+}
+
+#ifdef USE_POLL
+bool Sock::WaitPoll(std::chrono::milliseconds timeout, Event requested, Event* occurred) const
+{
     pollfd fd;
     fd.fd = m_socket;
     fd.events = 0;
@@ -179,7 +213,11 @@ bool Sock::Wait(std::chrono::milliseconds timeout, Event requested, Event* occur
     }
 
     return true;
-#else
+}
+#endif /* USE_POLL */
+
+bool Sock::WaitSelect(std::chrono::milliseconds timeout, Event requested, Event* occurred) const
+{
     if (!IsSelectableSocket(m_socket)) {
         return false;
     }
@@ -214,7 +252,6 @@ bool Sock::Wait(std::chrono::milliseconds timeout, Event requested, Event* occur
     }
 
     return true;
-#endif /* USE_POLL */
 }
 
 void Sock::SendComplete(const std::string& data,
