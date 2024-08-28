fa6dd58c8857106fa93657636ecb3b7db8caf926
pasta <pasta@dashboost.org>
2024-08-28 12:13:32 -0500

    feat: increase the number of block headers able to be downloaded at once to 8000 in protocol version `70234`

--- src/version.h
+++ src/version.h
@@ -11,7 +11,7 @@
  */
 
 
-static const int PROTOCOL_VERSION = 70233;
+static const int PROTOCOL_VERSION = 70234;
 
 //! initial proto version, to be increased after version/verack negotiation
 static const int INIT_PROTO_VERSION = 209;
@@ -54,6 +54,9 @@ static const int MNLISTDIFF_CHAINLOCKS_PROTO_VERSION = 70230;
 
 //! Legacy ISLOCK messages and a corresponding INV were dropped in this version
 static const int NO_LEGACY_ISLOCK_PROTO_VERSION = 70231;
+
+//! Maximum header count was increased from 2000 -> 8000 in this version
+static const int INCREASE_MAX_HEADERS_VERSION = 70234;
 
 // Make sure that none of the values above collide with `ADDRV2_FORMAT`.
 
