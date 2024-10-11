d706025dac9db5a64ce6af6705ffb621f6a38053
Kittywhiskers Van Gogh <63189531+kwvg@users.noreply.github.com>
2024-10-11 15:38:46 +0000

    refactor: move function definitions to source files
    This will force us to explicitly enumerate all the different managers
    that require CFlatDB

--- src/flatdb.h
+++ src/flatdb.h
@@ -5,23 +5,14 @@
 #ifndef BITCOIN_FLATDB_H
 #define BITCOIN_FLATDB_H
 
-#include <clientversion.h>
-#include <chainparams.h>
 #include <fs.h>
-#include <hash.h>
-#include <streams.h>
-#include <util/system.h>
 
-/**
-*   Generic Dumping and Loading
-*   ---------------------------
-*/
+#include <string>
 
-template<typename T>
+template <typename T>
 class CFlatDB
 {
 private:
-
     enum ReadResult {
         Ok,
         FileError,
@@ -36,176 +27,17 @@ private:
     std::string strFilename;
     std::string strMagicMessage;
 
-    bool CoreWrite(const T& objToSave)
-    {
-        // LOCK(objToSave.cs);
+    bool CoreWrite(const T& objToSave);
+    ReadResult CoreRead(T& objToLoad);
 
-        int64_t nStart = GetTimeMillis();
-
-        // serialize, checksum data up to that point, then append checksum
-        CDataStream ssObj(SER_DISK, CLIENT_VERSION);
-        ssObj << strMagicMessage; // specific magic message for this type of object
-        ssObj << Params().MessageStart(); // network specific magic number
-        ssObj << objToSave;
-        uint256 hash = Hash(ssObj);
-        ssObj << hash;
-
-        // open output file, and associate with CAutoFile
-        FILE *file = fsbridge::fopen(pathDB, "wb");
-        CAutoFile fileout(file, SER_DISK, CLIENT_VERSION);
-        if (fileout.IsNull())
-            return error("%s: Failed to open file %s", __func__, fs::PathToString(pathDB));
-
-        // Write and commit header, data
-        try {
-            fileout << ssObj;
-        }
-        catch (std::exception &e) {
-            return error("%s: Serialize or I/O error - %s", __func__, e.what());
-        }
-        fileout.fclose();
-
-        LogPrintf("Written info to %s  %dms\n", strFilename, GetTimeMillis() - nStart);
-        LogPrintf("     %s\n", objToSave.ToString());
-
-        return true;
-    }
-
-    ReadResult CoreRead(T& objToLoad)
-    {
-        //LOCK(objToLoad.cs);
-
-        int64_t nStart = GetTimeMillis();
-        // open input file, and associate with CAutoFile
-        FILE *file = fsbridge::fopen(pathDB, "rb");
-        CAutoFile filein(file, SER_DISK, CLIENT_VERSION);
-        if (filein.IsNull())
-        {
-            error("%s: Failed to open file %s", __func__, fs::PathToString(pathDB));
-            return FileError;
-        }
-
-        // use file size to size memory buffer
-        int fileSize = fs::file_size(pathDB);
-        int dataSize = fileSize - sizeof(uint256);
-        // Don't try to resize to a negative number if file is small
-        if (dataSize < 0)
-            dataSize = 0;
-        std::vector<unsigned char> vchData;
-        vchData.resize(dataSize);
-        uint256 hashIn;
-
-        // read data and checksum from file
-        try {
-            filein.read(MakeWritableByteSpan(vchData));
-            filein >> hashIn;
-        }
-        catch (std::exception &e) {
-            error("%s: Deserialize or I/O error - %s", __func__, e.what());
-            return HashReadError;
-        }
-        filein.fclose();
-
-        CDataStream ssObj(vchData, SER_DISK, CLIENT_VERSION);
-
-        // verify stored checksum matches input data
-        uint256 hashTmp = Hash(ssObj);
-        if (hashIn != hashTmp)
-        {
-            error("%s: Checksum mismatch, data corrupted", __func__);
-            return IncorrectHash;
-        }
-
-
-        try {
-            unsigned char pchMsgTmp[4];
-            std::string strMagicMessageTmp;
-            // de-serialize file header (file specific magic message) and ..
-            ssObj >> strMagicMessageTmp;
-
-            // ... verify the message matches predefined one
-            if (strMagicMessage != strMagicMessageTmp)
-            {
-                error("%s: Invalid magic message", __func__);
-                return IncorrectMagicMessage;
-            }
-
-
-            // de-serialize file header (network specific magic number) and ..
-            ssObj >> pchMsgTmp;
-
-            // ... verify the network matches ours
-            if (memcmp(pchMsgTmp, Params().MessageStart(), sizeof(pchMsgTmp)))
-            {
-                error("%s: Invalid network magic number", __func__);
-                return IncorrectMagicNumber;
-            }
-
-            // de-serialize data into T object
-            ssObj >> objToLoad;
-        }
-        catch (std::exception &e) {
-            objToLoad.Clear();
-            error("%s: Deserialize or I/O error - %s", __func__, e.what());
-            return IncorrectFormat;
-        }
-
-        LogPrintf("Loaded info from %s  %dms\n", strFilename, GetTimeMillis() - nStart);
-        LogPrintf("     %s\n", objToLoad.ToString());
-
-        return Ok;
-    }
-
-    bool Read(T& objToLoad)
-    {
-        ReadResult readResult = CoreRead(objToLoad);
-        if (readResult == FileError)
-            LogPrintf("Missing file %s, will try to recreate\n", strFilename);
-        else if (readResult != Ok)
-        {
-            LogPrintf("Error reading %s: ", strFilename);
-            if(readResult == IncorrectFormat)
-            {
-                LogPrintf("%s: Magic is ok but data has invalid format, will try to recreate\n", __func__);
-            }
-            else {
-                LogPrintf("%s: File format is unknown or invalid, please fix it manually\n", __func__);
-                // program should exit with an error
-                return false;
-            }
-        }
-        return true;
-    }
+    bool Read(T& objToLoad);
 
 public:
-    CFlatDB(std::string&& strFilenameIn, std::string&& strMagicMessageIn) :
-        pathDB{gArgs.GetDataDirNet() / strFilenameIn},
-        strFilename{strFilenameIn},
-        strMagicMessage{strMagicMessageIn}
-    {
-    }
+    CFlatDB(std::string&& strFilenameIn, std::string&& strMagicMessageIn);
+    ~CFlatDB();
 
-    bool Load(T& objToLoad)
-    {
-        LogPrintf("Reading info from %s...\n", strFilename);
-        return Read(objToLoad);
-    }
-
-    bool Store(const T& objToSave)
-    {
-        LogPrintf("Verifying %s format...\n", strFilename);
-        T tmpObjToLoad;
-        if (!Read(tmpObjToLoad)) return false;
-
-        int64_t nStart = GetTimeMillis();
-
-        LogPrintf("Writing info to %s...\n", strFilename);
-        CoreWrite(objToSave);
-        LogPrintf("%s dump finished  %dms\n", strFilename, GetTimeMillis() - nStart);
-
-        return true;
-    }
+    bool Load(T& objToLoad);
+    bool Store(const T& objToSave);
 };
-
 
 #endif // BITCOIN_FLATDB_H
--- src/test/util/setup_common.cpp
+++ src/test/util/setup_common.cpp
@@ -12,7 +12,6 @@
 #include <consensus/validation.h>
 #include <deploymentstatus.h>
 #include <crypto/sha256.h>
-#include <flatdb.h>
 #include <governance/governance.h>
 #include <index/txindex.h>
 #include <init.h>
