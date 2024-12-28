0a6a1dfafc04d03e926badcdc4744b3a1cdbef8e
Kittywhiskers Van Gogh <63189531+kwvg@users.noreply.github.com>
2024-12-28 14:00:07 +0000

    refactor: move oversized transaction check to tail end of scope
    Change needed so that next commit can extend check to include
    post-signature size.

--- src/wallet/wallet.cpp
+++ src/wallet/wallet.cpp
@@ -3720,12 +3720,6 @@ bool CWallet::CreateTransactionInternal(
                         nBytes += GetSizeOfCompactSize(nExtraPayloadSize) + nExtraPayloadSize;
                     }
 
-                    if (static_cast<size_t>(nBytes) > MAX_STANDARD_TX_SIZE) {
-                        // Do not create oversized transactions (bad-txns-oversize).
-                        error = _("Transaction too large");
-                        return false;
-                    }
-
                     // Remove scriptSigs to eliminate the fee calculation dummy signatures
                     for (auto& txin : txNew.vin) {
                         txin.scriptSig = CScript();
@@ -3886,6 +3880,12 @@ bool CWallet::CreateTransactionInternal(
 
         // Return the constructed transaction data.
         tx = MakeTransactionRef(std::move(txNew));
+
+        // Limit size
+        if (static_cast<size_t>(nBytes) > MAX_STANDARD_TX_SIZE) {
+            error = _("Transaction too large");
+            return false;
+        }
     }
 
     if (nFeeRet > m_default_max_tx_fee) {
