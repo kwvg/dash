902f94067f95ce43325e75215b92b0de7d2edf93
Kittywhiskers Van Gogh <63189531+kwvg@users.noreply.github.com>
2025-01-20 16:05:13 +0000

    meow

--- src/masternode/address.cpp
+++ src/masternode/address.cpp
@@ -7,6 +7,9 @@
 #include <bech32.h>
 #include <chainparams.h>
 #include <evo/deterministicmns.h>
+#include <netaddress.h>
+#include <protocol.h>
+#include <serialize.h>
 #include <util/check.h>
 #include <util/strencodings.h>
 
@@ -101,4 +104,147 @@ std::optional<CService> GetConnectionDetails(CDeterministicMNManager& dmnman, co
         return error(error_str, strprintf("Masternode not found in list"));
     }
     return Assert(mn->pdmnState)->addr;
+}
+
+// This doesn't do anything for now but should help us evaluate the kind of interface needed so we can
+// abstract currently deployed logic to resemble an interface that closely matches this one to make the
+// transition smoother.
+
+static constexpr uint8_t NETINFO_FORMAT_VERSION{1};
+
+template<> struct is_serializable_enum<CNetAddr::BIP155Network> : std::true_type {};
+
+enum class Purpose : uint8_t
+{
+    // Mandatory for all masternodes
+    CORE_P2P = 0x01,
+    // Mandatory for all EvoNodes
+    PLATFORM_P2P = 0x02,
+    // Optional for EvoNodes
+    PLATFORM_API = 0x03,
+
+    // Optional for all masternodes, available on devnet only. Does nothing for now.
+    RESERVED_TESTING = 0x00
+};
+template<> struct is_serializable_enum<Purpose> : std::true_type {};
+
+// All extensions should start with 0xDn where n is your extension number to avoid
+// conflicts with BIP155 network IDs.
+enum class Extensions : uint8_t
+{
+    // Extension A in Appendix C of DIP3. It rolls off the tongue easily, promise.
+    DOMAINS = 0xD0
+};
+template<> struct is_serializable_enum<Extensions> : std::true_type {};
+
+class MnNetInfo
+{
+private:
+    struct NetInfo
+    {
+        using NetAddrVariant = std::pair<CNetAddr::BIP155Network, CNetAddr>;
+        using StrAddrVariant = std::pair<Extensions, std::string>;
+
+        // Type of address, could be BIP155 type or an extension as defined in
+        // Appendix C DIP3 extension. Serialized as uint8_t.
+        std::variant<std::monostate, NetAddrVariant, StrAddrVariant> type_addr;
+        uint16_t port;
+
+        NetInfo() = default;
+        ~NetInfo() = default;
+
+        template<typename Stream>
+        void Serialize(Stream &s) const
+        {
+            s << type_addr.index();
+            switch(type_addr.index()) {
+            case 0 /* std::monotype */: return; // There's nothing more to serialize, bail out!
+            case 1 /* NetAddrVariant */: {
+                s << std::get<NetAddrVariant>(type_addr);
+                break;
+            }
+            case 2 /* StrAddrVariant */: {
+                s << std::get<StrAddrVariant>(type_addr);
+                break;
+            }
+            default: {
+                throw std::ios_base::failure("invalid variant");
+            }
+            }
+            s << port;
+        }
+
+        template<typename Stream>
+        void Unserialize(Stream &s)
+        {
+            Clear();
+            uint8_t type_idx;
+            s >> type_idx;
+            switch (type_idx) {
+            case 0 /* std::monotype */: return; // Invalid data, nothing more to read!
+            case 1 /* NetAddrVariant */: {
+                NetAddrVariant net_addr;
+                s >> net_addr;
+                type_addr = net_addr;
+                break;
+            }
+            case 2 /* StrAddrVariant */: {
+                StrAddrVariant str_addr;
+                s >> str_addr;
+                type_addr = str_addr;
+                break;
+            }
+            default: {
+                throw std::ios_base::failure("invalid variation byte");
+            }
+            }
+           s << port;
+        }
+
+        void Clear()
+        {
+            type_addr = std::monostate{};
+            port = 0;
+        }
+    };
+
+    struct NetEntry
+    {
+        Purpose purpose;
+        std::vector<NetInfo> entries;
+
+        NetEntry() = default;
+        ~NetEntry() = default;
+
+        SERIALIZE_METHODS(NetEntry, obj)
+        {
+            READWRITE(obj.purpose);
+            READWRITE(obj.entries);
+        }
+    };
+
+    // The format corresponds to the on-disk format *and* validation rules. Any changes
+    // to MnNetInfo, NetEntry, NetInfo, Purpose or Extensions will require incrementing
+    // this value.
+    uint8_t version{NETINFO_FORMAT_VERSION};
+    std::vector<NetEntry> data{};
+
+public:
+    MnNetInfo() = default;
+    ~MnNetInfo() = default;
+
+    SERIALIZE_METHODS(MnNetInfo, obj)
+    {
+        READWRITE(obj.version);
+        READWRITE(obj.data);
+    }
+};
+
+// Stub function to ensure compiler verifies serialization functions actually work
+void TestSerializationMagic()
+{
+    MnNetInfo network_info;
+    CDataStream s(SER_NETWORK, PROTOCOL_VERSION);
+    s << network_info;
+    s >> network_info;
 }
