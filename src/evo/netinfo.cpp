badeefe062a727aafe1c22ed5a1e1ad156e0a1fd
Kittywhiskers Van Gogh <63189531+kwvg@users.noreply.github.com>
2025-03-23 22:41:12 +0000

    refactor: move ProTx service validation to `MnNetInfo`
    Lets us additionally run static sanity checks during assignment.

--- src/evo/netinfo.cpp
+++ src/evo/netinfo.cpp
@@ -6,12 +6,52 @@
 
 #include <chainparams.h>
 #include <netbase.h>
+#include <util/system.h>
+
+namespace {
+static std::unique_ptr<const CChainParams> g_main_params{nullptr};
+
+const bool IsNodeOnMainnet() { return Params().NetworkIDString() == CBaseChainParams::MAIN; }
+const CChainParams& MainParams()
+{
+    // TODO: use real args here
+    if (!g_main_params) g_main_params = CreateChainParams(ArgsManager{}, CBaseChainParams::MAIN);
+    return *g_main_params;
+}
+} // anonymous namespace
+
+NetInfoStatus MnNetInfo::ValidateService(const CService& service)
+{
+    if (!service.IsValid()) {
+        return NetInfoStatus::BadInput;
+    }
+    if (!service.IsIPv4()) {
+        return NetInfoStatus::BadInput;
+    }
+    if (Params().RequireRoutableExternalIP() && !service.IsRoutable()) {
+        return NetInfoStatus::BadInput;
+    }
+
+    const auto default_port_main = MainParams().GetDefaultPort();
+    if (IsNodeOnMainnet() && service.GetPort() != default_port_main) {
+        /* Must use mainnet port on mainnet */
+        return NetInfoStatus::BadPort;
+    } else if (service.GetPort() == default_port_main) {
+        /* Using mainnet port prohibited outside of mainnet */
+        return NetInfoStatus::BadPort;
+    }
+
+    return NetInfoStatus::Success;
+}
 
 NetInfoStatus MnNetInfo::SetEntry(const std::string input)
 {
     if (auto service = Lookup(input, /*portDefault=*/Params().GetDefaultPort(), /*fAllowLookup=*/false); service.has_value()) {
-        addr = service.value();
-        return NetInfoStatus::Success;
+        const auto ret = ValidateService(service.value());
+        if (ret == NetInfoStatus::Success) {
+            addr = service.value();
+        }
+        return ret;
     }
     return NetInfoStatus::BadInput;
 }
