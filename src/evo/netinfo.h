a044be7c65492369f063868de912695088936830
Kittywhiskers Van Gogh <63189531+kwvg@users.noreply.github.com>
2025-05-03 15:18:32 +0000

    evo: prohibit overwriting entry in `MnNetInfo`

--- src/evo/netinfo.h
+++ src/evo/netinfo.h
@@ -13,6 +13,10 @@
 class CService;
 
 enum class NetInfoStatus : uint8_t {
+    // Adding entries
+    MaxLimit,
+
+    // Validation
     BadInput,
     BadPort,
     Malformed,
@@ -22,6 +26,8 @@ enum class NetInfoStatus : uint8_t {
 constexpr std::string_view NISToString(const NetInfoStatus code)
 {
     switch (code) {
+    case NetInfoStatus::MaxLimit:
+        return "too many entries";
     case NetInfoStatus::BadInput:
         return "invalid address";
     case NetInfoStatus::BadPort:
--- src/test/evo_netinfo_tests.cpp
+++ src/test/evo_netinfo_tests.cpp
@@ -57,6 +57,14 @@ BOOST_AUTO_TEST_CASE(mnnetinfo_rules)
             ValidateGetEntries(netInfo.GetEntries(), /*expected_size=*/1);
         }
     }
+
+    {
+        // MnNetInfo only stores one value, overwriting prohibited
+        MnNetInfo netInfo;
+        BOOST_CHECK_EQUAL(netInfo.AddEntry("1.1.1.1:8888"), NetInfoStatus::Success);
+        BOOST_CHECK_EQUAL(netInfo.AddEntry("1.1.1.2:8888"), NetInfoStatus::MaxLimit);
+        ValidateGetEntries(netInfo.GetEntries(), /*expected_size=*/1);
+    }
 }
 
 bool CheckIfSerSame(const CService& lhs, const MnNetInfo& rhs)
@@ -81,6 +89,7 @@ BOOST_AUTO_TEST_CASE(cservice_compatible)
 
     // Valid IPv4 address, default P2P port implied
     service = LookupNumeric("1.1.1.1", Params().GetDefaultPort());
+    netInfo.Clear();
     BOOST_CHECK_EQUAL(netInfo.AddEntry("1.1.1.1"), NetInfoStatus::Success);
     BOOST_CHECK(CheckIfSerSame(service, netInfo));
 
