1bd07dcea3eff3dbe42b3e9a3cd0537b3055f422
Kittywhiskers Van Gogh <63189531+kwvg@users.noreply.github.com>
2025-05-11 23:38:04 +0000

    evo: introduce `NetInfoInterface`, make `MnNetInfo` an implementation

--- src/evo/deterministicmns.h
+++ src/evo/deterministicmns.h
@@ -391,6 +391,7 @@ private:
         DMNL_NO_TEMPLATE(CBLSPublicKey);
         DMNL_NO_TEMPLATE(MnNetInfo);
         DMNL_NO_TEMPLATE(NetInfoEntry);
+        DMNL_NO_TEMPLATE(NetInfoInterface);
 #undef DMNL_NO_TEMPLATE
         return ::SerializeHash(v);
     }
