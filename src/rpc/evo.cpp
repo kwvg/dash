03a85cc066aec63d24b4d59a24ac7a212f3fe009
Kittywhiskers Van Gogh <63189531+kwvg@users.noreply.github.com>
2025-06-02 18:52:28 +0000

    rpc: rename `platform{HTTP,P2P}Port` input to `platform{HTTP,P2P}Addrs`

--- src/rpc/evo.cpp
+++ src/rpc/evo.cpp
@@ -170,32 +170,32 @@ static RPCArg GetRpcArg(const std::string& strParamName)
             {"platformNodeID", RPCArg::Type::STR, RPCArg::Optional::NO,
                 "Platform P2P node ID, derived from P2P public key."}
         },
-        {"platformP2PPort",
-            {"platformP2PPort", RPCArg::Type::ARR, RPCArg::Optional::NO,
+        {"platformP2PAddrs",
+            {"platformP2PAddrs", RPCArg::Type::ARR, RPCArg::Optional::NO,
                 "Array of addresses in the form \"ADDR:PORT\" used by Platform for peer-to-peer connection.\n"
                 "Must be unique on the network. Can be set to an empty string, which will require a ProUpServTx afterwards.",
                 {
                     {"address", RPCArg::Type::STR, RPCArg::Optional::NO, ""},
                 }}
         },
-        {"platformP2PPort_update",
-            {"platformP2PPort", RPCArg::Type::ARR, RPCArg::Optional::NO,
+        {"platformP2PAddrs_update",
+            {"platformP2PAddrs", RPCArg::Type::ARR, RPCArg::Optional::NO,
                 "Array of addresses in the form \"ADDR:PORT\" used by Platform for peer-to-peer connection.\n"
                 "Must be unique on the network.",
                 {
                     {"address", RPCArg::Type::STR, RPCArg::Optional::NO, ""},
                 }}
         },
-        {"platformHTTPPort",
-            {"platformHTTPPort", RPCArg::Type::ARR, RPCArg::Optional::NO,
+        {"platformHTTPAddrs",
+            {"platformHTTPAddrs", RPCArg::Type::ARR, RPCArg::Optional::NO,
                 "Array of addresses in the form \"ADDR:PORT\" used by Platform for their HTTP API.\n"
                 "Must be unique on the network. Can be set to an empty string, which will require a ProUpServTx afterwards.",
                 {
                     {"address", RPCArg::Type::STR, RPCArg::Optional::NO, ""},
                 }}
         },
-        {"platformHTTPPort_update",
-            {"platformHTTPPort", RPCArg::Type::ARR, RPCArg::Optional::NO,
+        {"platformHTTPAddrs_update",
+            {"platformHTTPAddrs", RPCArg::Type::ARR, RPCArg::Optional::NO,
                 "Array of addresses in the form \"ADDR:PORT\" used by Platform for their HTTP API.\n"
                 "Must be unique on the network.",
                 {
@@ -558,8 +558,8 @@ static RPCHelpMan protx_register_fund_evo()
             GetRpcArg("operatorReward"),
             GetRpcArg("payoutAddress_register"),
             GetRpcArg("platformNodeID"),
-            GetRpcArg("platformP2PPort"),
-            GetRpcArg("platformHTTPPort"),
+            GetRpcArg("platformP2PAddrs"),
+            GetRpcArg("platformHTTPAddrs"),
             GetRpcArg("fundAddress"),
             GetRpcArg("submit"),
         },
@@ -597,8 +597,8 @@ static RPCHelpMan protx_register_evo()
             GetRpcArg("operatorReward"),
             GetRpcArg("payoutAddress_register"),
             GetRpcArg("platformNodeID"),
-            GetRpcArg("platformP2PPort"),
-            GetRpcArg("platformHTTPPort"),
+            GetRpcArg("platformP2PAddrs"),
+            GetRpcArg("platformHTTPAddrs"),
             GetRpcArg("feeSourceAddress"),
             GetRpcArg("submit"),
         },
@@ -635,8 +635,8 @@ static RPCHelpMan protx_register_prepare_evo()
             GetRpcArg("operatorReward"),
             GetRpcArg("payoutAddress_register"),
             GetRpcArg("platformNodeID"),
-            GetRpcArg("platformP2PPort"),
-            GetRpcArg("platformHTTPPort"),
+            GetRpcArg("platformP2PAddrs"),
+            GetRpcArg("platformHTTPAddrs"),
             GetRpcArg("feeSourceAddress"),
         },
         RPCResult{
@@ -944,8 +944,8 @@ static RPCHelpMan protx_update_service_evo()
             GetRpcArg("coreP2PAddrs_update"),
             GetRpcArg("operatorKey"),
             GetRpcArg("platformNodeID"),
-            GetRpcArg("platformP2PPort_update"),
-            GetRpcArg("platformHTTPPort_update"),
+            GetRpcArg("platformP2PAddrs_update"),
+            GetRpcArg("platformHTTPAddrs_update"),
             GetRpcArg("operatorPayoutAddress"),
             GetRpcArg("feeSourceAddress"),
         },
--- test/functional/rpc_netinfo.py
+++ test/functional/rpc_netinfo.py
@@ -235,15 +235,15 @@ class NetInfoTest(BitcoinTestFramework):
         self.test_deprecation()
 
     def test_validation_common(self):
-        # platformP2PPort and platformHTTPPort must be within acceptable range (i.e. a valid port number)
+        # platformP2PAddrs and platformHTTPAddrs must be within acceptable range (i.e. a valid port number)
         self.node_evo.register_mn(self, False, f"127.0.0.1:{self.node_evo.port_p2p}", "0", DEFAULT_PORT_PLATFORM_HTTP,
-                                  -8, "platformP2PPort must be a valid port [1-65535]")
+                                  -8, "platformP2PAddrs must be a valid port [1-65535]")
         self.node_evo.register_mn(self, False, f"127.0.0.1:{self.node_evo.port_p2p}", "65536", DEFAULT_PORT_PLATFORM_HTTP,
-                                  -8, "platformP2PPort must be a valid port [1-65535]")
+                                  -8, "platformP2PAddrs must be a valid port [1-65535]")
         self.node_evo.register_mn(self, False, f"127.0.0.1:{self.node_evo.port_p2p}", DEFAULT_PORT_PLATFORM_P2P, "0",
-                                  -8, "platformHTTPPort must be a valid port [1-65535]")
+                                  -8, "platformHTTPAddrs must be a valid port [1-65535]")
         self.node_evo.register_mn(self, False, f"127.0.0.1:{self.node_evo.port_p2p}", DEFAULT_PORT_PLATFORM_P2P, "65536",
-                                  -8, "platformHTTPPort must be a valid port [1-65535]")
+                                  -8, "platformHTTPAddrs must be a valid port [1-65535]")
 
     def test_validation_legacy(self):
         # Using mainnet P2P port gets refused
@@ -256,15 +256,15 @@ class NetInfoTest(BitcoinTestFramework):
                                   DEFAULT_PORT_PLATFORM_P2P, DEFAULT_PORT_PLATFORM_HTTP,
                                   -8, f"Error setting coreP2PAddrs[1] to '127.0.0.2:{self.node_evo.port_p2p}' (too many entries)")
 
-        # platformP2PPort and platformHTTPPort doesn't accept non-numeric inputs
+        # platformP2PAddrs and platformHTTPAddrs doesn't accept non-numeric inputs
         self.node_evo.register_mn(self, False, f"127.0.0.1:{self.node_evo.port_p2p}", f"127.0.0.1:{DEFAULT_PORT_PLATFORM_P2P}", DEFAULT_PORT_PLATFORM_HTTP,
-                                  -8, "ProTx version disallows storing addresses in platformP2PPort (must specify port number only)")
+                                  -8, "ProTx version disallows storing addresses in platformP2PAddrs (must specify port number only)")
         self.node_evo.register_mn(self, False, f"127.0.0.1:{self.node_evo.port_p2p}", [f"127.0.0.1:{DEFAULT_PORT_PLATFORM_P2P}"], DEFAULT_PORT_PLATFORM_HTTP,
-                                  -8, "ProTx version disallows storing addresses in platformP2PPort (must specify port number only)")
+                                  -8, "ProTx version disallows storing addresses in platformP2PAddrs (must specify port number only)")
         self.node_evo.register_mn(self, False, f"127.0.0.1:{self.node_evo.port_p2p}", DEFAULT_PORT_PLATFORM_P2P, f"127.0.0.1:{DEFAULT_PORT_PLATFORM_HTTP}",
-                                  -8, "ProTx version disallows storing addresses in platformHTTPPort (must specify port number only)")
+                                  -8, "ProTx version disallows storing addresses in platformHTTPAddrs (must specify port number only)")
         self.node_evo.register_mn(self, False, f"127.0.0.1:{self.node_evo.port_p2p}", DEFAULT_PORT_PLATFORM_P2P, [f"127.0.0.1:{DEFAULT_PORT_PLATFORM_HTTP}"],
-                                  -8, "ProTx version disallows storing addresses in platformHTTPPort (must specify port number only)")
+                                  -8, "ProTx version disallows storing addresses in platformHTTPAddrs (must specify port number only)")
 
     def test_deprecation(self):
         # netInfo is represented with JSON in CProRegTx, CProUpServTx, CDeterministicMNState and CSimplifiedMNListEntry,
