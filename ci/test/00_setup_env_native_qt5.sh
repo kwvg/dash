4354c8f093dd9e6e298806b67ddce5a6ce74cff7
Kittywhiskers Van Gogh <63189531+kwvg@users.noreply.github.com>
2025-11-19 14:22:19 +0530

    ci: build `linux64` depends with lowest tested GCC version
    Since we're using Nix supplied GCC, they don't share the same standard
    libraries, they are forwards compatible, not back, so compile against
    oldest tested version to avoid breakage.

--- ci/test/00_setup_env_native_qt5.sh
+++ ci/test/00_setup_env_native_qt5.sh
@@ -9,10 +9,10 @@ export LC_ALL=C.UTF-8
 export CONTAINER_NAME=ci_native_qt5
 export HOST=x86_64-pc-linux-gnu
 export PACKAGES="python3-zmq qtbase5-dev qttools5-dev-tools libdbus-1-dev libharfbuzz-dev"
-export DEP_OPTS=""
+export DEP_OPTS="CC=gcc-11 CXX=g++-11"
 export TEST_RUNNER_EXTRA="--previous-releases --coverage --extended --exclude feature_pruning,feature_dbcrash"  # Run extended tests so that coverage does not fail, but exclude the very slow dbcrash
 export RUN_UNIT_TESTS_SEQUENTIAL="true"
 export RUN_UNIT_TESTS="false"
 export GOAL="install"
 export DOWNLOAD_PREVIOUS_RELEASES="true"
-export BITCOIN_CONFIG="--enable-zmq --with-libs=no --enable-reduce-exports LDFLAGS=-static-libstdc++ --with-boost-process"
+export BITCOIN_CONFIG="--enable-zmq --with-libs=no --enable-reduce-exports LDFLAGS=-static-libstdc++ --with-boost-process CC=gcc CXX=g++"
