b621b4011327f070cc1b9f1eddd4193996ae2533
Kittywhiskers Van Gogh <63189531+kwvg@users.noreply.github.com>
2025-12-06 00:16:29 +0530

    refactor: pull data recovery lambda to a dedicated function
    This is done to remove m_mn_activeman calls from within the worker
    to allow us to run it in watch-only mode.

--- src/llmq/participant.h
+++ src/llmq/participant.h
@@ -75,9 +75,11 @@ private:
     size_t GetQuorumRecoveryStartOffset(const CQuorum& quorum, gsl::not_null<const CBlockIndex*> pIndex) const;
 
     void CheckQuorumConnections(CConnman& connman, const Consensus::LLMQParams& llmqParams, gsl::not_null<const CBlockIndex*> pindexNew) const;
+    void DataRecoveryThread(CConnman& connman, gsl::not_null<const CBlockIndex*> block_index, CQuorumCPtr quorum, uint16_t data_mask,
+                            const uint256& protx_hash, size_t start_offset) const;
     void StartCleanupOldQuorumDataThread(gsl::not_null<const CBlockIndex*> pIndex) const;
-    void StartQuorumDataRecoveryThread(CConnman& connman, CQuorumCPtr pQuorum, gsl::not_null<const CBlockIndex*> pIndex,
-                                       uint16_t nDataMask) const;
+    void StartDataRecoveryThread(CConnman& connman, CQuorumCPtr pQuorum, gsl::not_null<const CBlockIndex*> pIndex,
+                                 uint16_t nDataMask) const;
     void TriggerQuorumDataRecoveryThreads(CConnman& connman, gsl::not_null<const CBlockIndex*> pIndex) const;
 };
 } // namespace llmq
