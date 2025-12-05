56a3992f083932df48830ca78703224de1b00b6b
Kittywhiskers Van Gogh <63189531+kwvg@users.noreply.github.com>
2025-12-06 04:06:05 +0530

    refactor: separate observer/participant logic into dedicated class

---
+++ src/llmq/participant.h
@@ -0,0 +1,85 @@
+// Copyright (c) 2018-2025 The Dash Core developers
+// Distributed under the MIT software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+#ifndef BITCOIN_LLMQ_PARTICIPANT_H
+#define BITCOIN_LLMQ_PARTICIPANT_H
+
+#include <bls/bls.h>
+#include <llmq/quorums.h>
+#include <llmq/types.h>
+#include <msg_result.h>
+
+#include <consensus/params.h>
+#include <saltedhasher.h>
+#include <span.h>
+#include <sync.h>
+#include <threadsafety.h>
+#include <uint256.h>
+
+#include <map>
+
+class CActiveMasternodeManager;
+class CBlockIndex;
+class CBLSWorker;
+class CConnman;
+class CDeterministicMNManager;
+class CDKGSessionManager;
+class CNode;
+class CSporkManager;
+
+namespace llmq {
+class CQuorum;
+class CQuorumDataRequest;
+class CQuorumManager;
+class CQuorumSnapshotManager;
+
+class QuorumParticipant
+{
+private:
+    CBLSWorker& m_bls_worker;
+    CDeterministicMNManager& m_dmnman;
+    CQuorumManager& m_qman;
+    CQuorumSnapshotManager& m_qsnapman;
+    const CActiveMasternodeManager* const m_mn_activeman;
+    const CMasternodeSync& m_mn_sync;
+    const CSporkManager& m_sporkman;
+    const bool m_quorums_recovery{false};
+    const bool m_quorums_watch{false};
+
+private:
+    mutable Mutex cs_cleanup;
+    mutable std::map<Consensus::LLMQType, Uint256LruHashMap<uint256>> cleanupQuorumsCache GUARDED_BY(cs_cleanup);
+
+public:
+    QuorumParticipant() = delete;
+    QuorumParticipant(const QuorumParticipant&) = delete;
+    QuorumParticipant& operator=(const QuorumParticipant&) = delete;
+    explicit QuorumParticipant(CBLSWorker& bls_worker, CDeterministicMNManager& dmnman, CQuorumManager& qman,
+                               CQuorumSnapshotManager& qsnapman, const CActiveMasternodeManager* const mn_activeman,
+                               const CMasternodeSync& mn_sync, const CSporkManager& sporkman, bool quorums_recovery, bool quorums_watch);
+    ~QuorumParticipant();
+
+    bool SetQuorumSecretKeyShare(CQuorum& quorum, Span<CBLSSecretKey> skContributions) const;
+
+    [[nodiscard]] MessageProcessingResult ProcessEncryptedContribs(CNode& pfrom, CConnman& connman, bool request_limit_exceeded,
+                                                                   CDataStream& vStream, CQuorum& quorum, CQuorumDataRequest& request,
+                                                                   const CBlockIndex* const block_index, std::string_view msg_type);
+
+    void UpdatedBlockTip(const CBlockIndex* pindexNew, CConnman& connman, bool fInitialDownload) const;
+
+private:
+    /// Returns the start offset for the masternode with the given proTxHash. This offset is applied when picking data recovery members of a quorum's
+    /// memberlist and is calculated based on a list of all member of all active quorums for the given llmqType in a way that each member
+    /// should receive the same number of request if all active llmqType members requests data from one llmqType quorum.
+    size_t GetQuorumRecoveryStartOffset(const CQuorum& quorum, gsl::not_null<const CBlockIndex*> pIndex) const;
+
+    void CheckQuorumConnections(CConnman& connman, const Consensus::LLMQParams& llmqParams, gsl::not_null<const CBlockIndex*> pindexNew) const;
+    void StartCleanupOldQuorumDataThread(gsl::not_null<const CBlockIndex*> pIndex) const;
+    void StartQuorumDataRecoveryThread(CConnman& connman, CQuorumCPtr pQuorum, gsl::not_null<const CBlockIndex*> pIndex,
+                                       uint16_t nDataMask) const;
+    void TriggerQuorumDataRecoveryThreads(CConnman& connman, gsl::not_null<const CBlockIndex*> pIndex) const;
+};
+} // namespace llmq
+
+#endif // BITCOIN_LLMQ_PARTICIPANT_H
