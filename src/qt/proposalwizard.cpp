45998cd3fc81db47eb73729f25bc3bcfdbcbd634
Kittywhiskers Van Gogh <63189531+kwvg@users.noreply.github.com>
2026-01-21 00:17:54 +0530

    qt: use `SendConfirmationDialog` instead of `QMessageBox` for confirm
    The visual language should match other coin-spending activities, also
    warn that that you don't get the fee back.

--- src/qt/proposalwizard.cpp
+++ src/qt/proposalwizard.cpp
@@ -20,6 +20,7 @@
 #include <qt/optionsmodel.h>
 #include <qt/qvalidatedlineedit.h>
 #include <qt/rpcconsole.h>
+#include <qt/sendcoinsdialog.h>
 #include <qt/walletmodel.h>
 
 #include <QDateTime>
@@ -230,11 +231,15 @@ void ProposalWizard::onPrepare()
     WalletModel::UnlockContext ctx(m_walletModel->requestUnlock());
     if (!ctx.isValid()) return;
 
-    // Confirm burn
-    if (QMessageBox::question(this, tr("Burn %1").arg(m_fee_formatted),
-                              tr("Burn %1 to create the fee transaction?").arg(m_fee_formatted),
-                              QMessageBox::StandardButton::Cancel | QMessageBox::StandardButton::Yes,
-                              QMessageBox::StandardButton::Cancel) != QMessageBox::StandardButton::Yes) {
+    SendConfirmationDialog diag_confirm(
+        /*title=*/tr("Confirm Proposal"),
+        /*question=*/tr("Are you sure you want to create this proposal?"),
+        /*details=*/tr("Creating proposals send %1 to the network. This fee is non-refundable regardless of outcome.").arg(m_fee_formatted),
+        /*informative_text=*/"", SEND_CONFIRM_DELAY,
+        /*enable_send=*/true, /*always_show_unsigned=*/false, this);
+    diag_confirm.setWindowModality(Qt::WindowModal);
+    diag_confirm.exec();
+    if (diag_confirm.result() != QMessageBox::Yes) {
         return;
     }
 
